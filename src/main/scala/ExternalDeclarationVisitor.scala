0e0b6e0358c4b11584e937824104faa25c31b891
takesano <takesano@yahoo-corp.jp>
2015-04-28 15:30:53 +0900

    code slim-up.

--- src/main/scala/ExternalDeclarationVisitor.scala
+++ src/main/scala/ExternalDeclarationVisitor.scala
@@ -14,39 +14,27 @@ trait ExternalDeclarationVisitor extends Converter {
 
   override def visitClass_interface(ctx: Class_interfaceContext): String = {
     val sb = new StringBuilder()
-    sb.append("class " + ctx.class_name.getText)
 
+    // class [CLASS-NAME] : [SUPERCLASS], [PROTOCOL1, PROTOCOL2, ...]
+    sb.append("class " + visit(ctx.class_name))
     if(ctx.superclass_name() != null) {
       sb.append(" : ")
-      sb.append(ctx.superclass_name().getText)
+      sb.append(visit(ctx.superclass_name))
     }
     if(ctx.protocol_reference_list() != null) {
-      val protocols = ctx.protocol_reference_list()
-        .protocol_list()
-        .children
-        .filter(_.isInstanceOf[Protocol_nameContext])
-        .map(_.getText)
-      sb.append(", " + protocols.mkString(", "))
+      sb.append(", " + visit(ctx.protocol_reference_list))
     }
 
+    // implementation of class
     sb.append(" {\n")
     if(ctx.interface_declaration_list() != null) {
-      val result = visit(ctx.interface_declaration_list())
-      if(result != null) {
-        sb.append(result)
-      }
+      sb.append(visit(ctx.interface_declaration_list()))
     }
 
     val implCtx = findCorrespondingClassImplementation(ctx)
-    if(implCtx != null) {
-      Option(implCtx.implementation_definition_list()) match {
-        case Some(c) =>
-          visit(c) match {
-            case s: String => sb.append(s)
-            case _ =>
-          }
-        case _ =>
-      }
+    if(implCtx != null && !visited.get(implCtx)) {
+      visited.put(implCtx, true)
+      sb.append(visit(implCtx.implementation_definition_list()))
     }
 
     sb.append("\n}")
@@ -56,23 +44,16 @@ trait ExternalDeclarationVisitor extends Converter {
 
   override def visitCategory_interface(ctx: Category_interfaceContext): String = {
     val sb = new StringBuilder()
-    sb.append("extension " + ctx.class_name.getText)
 
+    // extension [CLASS-NAME]
+    sb.append("extension " + visit(ctx.class_name))
     if(ctx.protocol_reference_list() != null) {
-      val protocols = ctx.protocol_reference_list()
-        .protocol_list()
-        .children
-        .filter(_.isInstanceOf[Protocol_nameContext])
-        .map(_.getText)
-      sb.append(" : " + protocols.mkString(", "))
+      sb.append(", " + visit(ctx.protocol_reference_list))
     }
 
     sb.append(" {\n")
     if(ctx.interface_declaration_list() != null) {
-      val result = visit(ctx.interface_declaration_list())
-      if(result != null) {
-        sb.append(result)
-      }
+      sb.append(visit(ctx.interface_declaration_list()))
     }
     sb.append("}")
 
@@ -94,10 +75,15 @@ trait ExternalDeclarationVisitor extends Converter {
     ""
   }
 
-  override def visitImplementation_definition_list(ctx: Implementation_definition_listContext): String = {
-    concatChildResults(ctx, "")
-  }
+  override def visitImplementation_definition_list(ctx: Implementation_definition_listContext) = concatChildResults(ctx, "")
 
-  override def visitProtocol_name(ctx: Protocol_nameContext) = ctx.getText
   override def visitClass_name(ctx: Class_nameContext) = ctx.getText
+  override def visitSuperclass_name(ctx: Superclass_nameContext) = ctx.getText
+  override def visitCategory_name(ctx: Category_nameContext) = ctx.getText
+
+  override def visitProtocol_reference_list(ctx: Protocol_reference_listContext) = visit(ctx.protocol_list)
+  override def visitProtocol_list(ctx: Protocol_listContext) = {
+    ctx.protocol_name.map(visit).mkString(", ")
+  }
+  override def visitProtocol_name(ctx: Protocol_nameContext) = ctx.getText
 }
