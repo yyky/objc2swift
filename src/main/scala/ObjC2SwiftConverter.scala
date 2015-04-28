5efa219b9e9b0848ce4dcb15cf7582f9463f54d5
takesano <takesano@yahoo-corp.jp>
2015-04-28 12:15:36 +0900

    split statement visitor.

--- src/main/scala/ObjC2SwiftConverter.scala
+++ src/main/scala/ObjC2SwiftConverter.scala
@@ -18,7 +18,8 @@ class ObjC2SwiftConverter(_root: Translation_unitContext)
   with ExternalDeclarationVisitor
   with PropertyVisitor
   with MethodVisitor
-  with DeclarationVisitor {
+  with DeclarationVisitor
+  with StatementVisitor {
 
   val root = _root
 
@@ -61,27 +62,6 @@ class ObjC2SwiftConverter(_root: Translation_unitContext)
       case s if s != "" => s
       case _ => "AnyObject"
     }
-
-  override def visitCompound_statement(ctx: Compound_statementContext): String = {
-    concatChildResults(ctx, "")
-  }
-
-  override def visitStatement_list(ctx: Statement_listContext): String = {
-    concatChildResults(ctx, "\n")
-  }
-
-  override def visitStatement(ctx: StatementContext): String = {
-    indent(ctx) + concatChildResults(ctx, " ") // TODO
-  }
-
-
-  override def visitJump_statement(ctx: Jump_statementContext): String = {
-    ctx.getChild(0).getText match {
-      case "return" => "return " + visit(ctx.expression)
-      case "break" => "" // TODO not implemented
-      case _ => "" // TODO
-    }
-  }
 
   /*
      * Expressions
@@ -129,61 +109,6 @@ class ObjC2SwiftConverter(_root: Translation_unitContext)
 
   override def visitExpression(ctx: ExpressionContext): String = {
     concatChildResults(ctx, "")
-  }
-
-  override def visitSelection_statement(ctx: Selection_statementContext): String = {
-    val sb = new StringBuilder()
-    var statement_kind = ""
-
-    for (element <- ctx.children) {
-      element match {
-        case symbol: TerminalNode => {
-          symbol.getSymbol.getText match {
-            case s if s == "if" || s == "switch" => {
-              sb.append(s)
-              statement_kind = s
-            }
-            case "(" | ")" => sb.append(" ")
-            case _ => null
-          }
-        }
-        case expression: ExpressionContext => {
-          sb.append(visit(expression))
-        }
-        case statement: StatementContext => {
-          sb.append("{\n")
-
-          if (statement_kind == "if") {
-            sb.append(indentString)
-          }
-          sb.append(visitChildren(statement) + "\n")
-          sb.append(indent(statement) +  "}\n")
-        }
-        case _ => null
-      }
-    }
-
-    sb.toString()
-  }
-
-  override def visitLabeled_statement(ctx: Labeled_statementContext): String = {
-    val sb = new StringBuilder()
-
-    for (element <- ctx.children) {
-      element match {
-        case symbol: TerminalNode => {
-          symbol.getSymbol.getText match {
-            case "case" => sb.append("case ")
-            case "default" => sb.append("default")
-            case ":" => sb.append(":\n" + indentString)
-            case _ => null
-          }
-        }
-        case _ => sb.append(visit(element))
-        //TODO fix indent bug
-      }
-    }
-    sb.toString()
   }
 
   override def visitEquality_expression(ctx: Equality_expressionContext): String = {
---
+++ src/main/scala/StatementVisitor.scala
@@ -0,0 +1,92 @@
+/**
+ * This file is part of objc2swift.
+ * https://github.com/yahoojapan/objc2swift
+ *
+ * Copyright (c) 2015 Yahoo Japan Corporation
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import ObjCParser._
+import org.antlr.v4.runtime.tree.TerminalNode
+import collection.JavaConversions._
+
+trait StatementVisitor extends Converter {
+  self: ObjCBaseVisitor[String] =>
+
+  override def visitStatement_list(ctx: Statement_listContext): String = {
+    concatChildResults(ctx, "\n")
+  }
+
+  override def visitStatement(ctx: StatementContext): String = {
+    indent(ctx) + concatChildResults(ctx, " ") // TODO
+  }
+
+  override def visitCompound_statement(ctx: Compound_statementContext): String = {
+    concatChildResults(ctx, "")
+  }
+
+  override def visitJump_statement(ctx: Jump_statementContext): String = {
+    ctx.getChild(0).getText match {
+      case "return" => "return " + visit(ctx.expression)
+      case "break" => "" // TODO not implemented
+      case _ => "" // TODO
+    }
+  }
+
+  override def visitSelection_statement(ctx: Selection_statementContext): String = {
+    val sb = new StringBuilder()
+    var statement_kind = ""
+
+    for (element <- ctx.children) {
+      element match {
+        case symbol: TerminalNode => {
+          symbol.getSymbol.getText match {
+            case s if s == "if" || s == "switch" => {
+              sb.append(s)
+              statement_kind = s
+            }
+            case "(" | ")" => sb.append(" ")
+            case _ => null
+          }
+        }
+        case expression: ExpressionContext => {
+          sb.append(visit(expression))
+        }
+        case statement: StatementContext => {
+          sb.append("{\n")
+
+          if (statement_kind == "if") {
+            sb.append(indentString)
+          }
+          sb.append(visitChildren(statement) + "\n")
+          sb.append(indent(statement) +  "}\n")
+        }
+        case _ => null
+      }
+    }
+
+    sb.toString()
+  }
+
+  override def visitLabeled_statement(ctx: Labeled_statementContext): String = {
+    val sb = new StringBuilder()
+
+    for (element <- ctx.children) {
+      element match {
+        case symbol: TerminalNode => {
+          symbol.getSymbol.getText match {
+            case "case" => sb.append("case ")
+            case "default" => sb.append("default")
+            case ":" => sb.append(":\n" + indentString)
+            case _ => null
+          }
+        }
+        case _ => sb.append(visit(element))
+        //TODO fix indent bug
+      }
+    }
+    sb.toString()
+  }
+}
