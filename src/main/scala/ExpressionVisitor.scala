8becd48bdd0acf70c54805ca0f6cea5dfce2fd5a
Yuya Kamada <ykamada@yahoo-corp.jp>
2015-04-28 10:45:03 +0900

    Add stringWithFormat() conversion logic

--- src/main/scala/ExpressionVisitor.scala
+++ src/main/scala/ExpressionVisitor.scala
@@ -72,6 +72,35 @@ trait ExpressionVisitor extends Converter {
     sb.toString()
   }
 
+  /*
+   * Expressions
+   */
+  /*
+  override def visitMessage_expression(ctx: Message_expressionContext): String = {
+    val sel = ctx.message_selector()
+    val rcv = visit(ctx.receiver)
+
+    // TODO: stringWithFormat conversion
+    rcv match {
+      case "NSString" =>
+        Option(sel.keyword_argument()) match {
+          case Some(k) if k.length != 0 =>
+            val p1 = k.get(0)
+            Option(p1.selector()) match {
+              case None => commonMessageExpression(ctx)
+              case Some(s) => s.getText match {
+                case s2 if s2 == "stringWithFormat" => stringWithFormat(sel)
+                case _ => commonMessageExpression(ctx)
+              }
+            }
+          case _ => commonMessageExpression(ctx)
+        }
+      case _ => commonMessageExpression(ctx)
+    }
+
+  }
+  */
+
   override def visitPrimary_expression(ctx: Primary_expressionContext): String = {
     // TODO need to support more
 
