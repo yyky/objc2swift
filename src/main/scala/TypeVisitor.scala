4f83f841ffdb6d1ac345c10e3ec09d9d4dbe5aeb
Hayato Yokota <hyokota@yahoo-corp.jp>
2015-05-08 15:12:32 +0900

    Refactor type specifier visitor

--- src/main/scala/TypeVisitor.scala
+++ src/main/scala/TypeVisitor.scala
@@ -9,6 +9,7 @@
  */
 
 import ObjCParser._
+import org.antlr.v4.runtime.tree.TerminalNode
 import collection.JavaConversions._
 
 trait TypeVisitor extends Converter {
@@ -23,24 +24,48 @@ trait TypeVisitor extends Converter {
    * TODO: Implement other types
    *
    */
-  override def visitType_specifier(ctx: Type_specifierContext): String =
-    (ctx.pointer match {
-      case null => ctx.getText
-      case _ => ctx.IDENTIFIER.getText
-    }) match {
-      case "void"   => "void"
-      case "id"     => "AnyObject"
-      case "short"  => "Int8"
-      case "int"    => "Int32"
-      case "long"   => "Int32"
-      case "float"  => "Float"
-      case "double" => "Double"
-      case "NSInteger" | "NSUInteger" => "Int"
-      case "NSArray" => "[AnyObject]"
-      case "NSDictionary" => "[NSObject : AnyObject]"
-      case "SEL" => "Selector"
-      case "BOOL" => "Bool"
-      case s if s != "" => s
-      case _ => "AnyObject"
+  override def visitType_specifier(ctx: Type_specifierContext): String = {
+    val sb = new StringBuilder()
+
+    for (element <- ctx.children) {
+      element match {
+        case symbol: TerminalNode => {
+          sb.append(
+            symbol.getSymbol.getText match {
+              case "void"   => "void"
+              case "id"     => "AnyObject"
+              case "short"  => "Int8"
+              case "int"    => "Int32"
+              case "long"   => "Int32"
+              case "float"  => "Float"
+              case "double" => "Double"
+              case s if s != "" => s
+              case _ => "AnyObject"
+            }
+          )
+        }
+        case className: Class_nameContext => {
+          sb.append(
+            className.getText match {
+              case "NSInteger" | "NSUInteger" => "Int"
+              case "NSArray" => "[AnyObject]"
+              case "NSDictionary" => "[NSObject : AnyObject]"
+              case "SEL" => "Selector"
+              case "BOOL" => "Bool"
+              case s if s != "" => s
+              case _ => "AnyObject"
+            }
+          )
+        }
+        case pointer: PointerContext => {
+          sb.append(visit(pointer))
+        }
+        case _ => {
+          sb.append(element.getText)
+        }
+      }
     }
+
+    sb.toString()
+  }
 }
