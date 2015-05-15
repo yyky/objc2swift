8e162e90fef77c2250c0f25ca30b4411642e150b
takesano <takesano@yahoo-corp.jp>
2015-05-15 15:03:33 +0900

    improve code.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,7 +18,9 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main {
   def main(args: Array[String]) {
-    val fileNames = args.filter(!_.startsWith("-"))
+    val options = Map("tree" -> args.contains("-t"))
+
+    val fileNames = args.filter(!_.startsWith("-")).toList
     if(fileNames.length == 0) {
       println("error: no input file specified.")
       return
@@ -30,8 +32,6 @@ object Main {
       return
     }
 
-    val options = Map("tree" -> args.contains("-t"))
-
     val streams = files.map(new FileInputStream(_))
     val seqStream = new SequenceInputStream(streams.toIterator)
     val input = new ANTLRInputStream(seqStream)
@@ -42,39 +42,15 @@ object Main {
 
     val root = parser.translation_unit
     val converter = new ObjC2SwiftConverter(root)
+
     val result = converter.getResult
+    val header = getHeader(files, options, parser, root)
 
-    output(result, files, options, parser, root)
+    println(s"$header\n$result")
   }
 
-  def output(result: String, files: Array[File], options: Map[String, Any], parser: Parser, root: ParserRuleContext) {
-    println("/* Hello Swift, Goodbye Obj-C.")
-    println(" * converted by 'objc2swift' https://github.com/yahoojapan/objc2swift")
-    println(" *")
-    println(" * source: " + files.mkString(", "))
-
-    if(options("tree") == true) {
-      println(" * source-tree:")
-      new ParseTreeWalker().walk(new ObjCBaseListener() {
-        override def enterEveryRule(ctx: ParserRuleContext): Unit = {
-          print(" * " + (ctx.depth - 1) + "  " * ctx.depth)
-          print(parser.getRuleNames()(ctx.getRuleIndex) + ": ")
-          print("'" + ctx.getStart.getText.replace("\n\r\t", " ") + "'")
-          if(ctx.getStart != ctx.getStop){
-            print(" - " + "'" + ctx.getStop.getText.replace("\n\r\t", " ") + "'")
-          }
-          println()
-        }
-      }, root)
-    }
-
-    println(" */")
-    println()
-    println(result)
-  }
-
-  def findFiles(input:Array[String]): Array[File] = {
-    val result = ArrayBuffer.empty[File]
+  def findFiles(input:List[String]): List[File] = {
+    val result = List.newBuilder[File]
 
     input.foreach({
       case x if x.contains("*") =>
@@ -86,8 +62,8 @@ object Main {
         val matcher = getName(x)
         val files = dir.listFiles(new FilenameFilter {
           override def accept(dir: File, name: String): Boolean = {
-              val extension = getExtension(name)
-              List("h", "m").contains(extension) && wildcardMatch(name, matcher)
+            val extension = getExtension(name)
+            List("h", "m").contains(extension) && wildcardMatch(name, matcher)
           }
         })
         result ++= files
@@ -100,6 +76,28 @@ object Main {
         }
     })
 
-    result.toArray
+    result.result
+  }
+
+  def getHeader(files: List[File], options: Map[String, Any], parser: Parser, root: ParserRuleContext): String = {
+    val lines = List.newBuilder[String]
+
+    lines += "Hello Swift, Goodbye Obj-C."
+    lines += "converted by 'objc2swift' https://github.com/yahoojapan/objc2swift"
+    lines += "original source: " + files.mkString(", ")
+
+    if(options("tree") == true) {
+      lines += ""
+      lines += "source-tree:"
+      new ParseTreeWalker().walk(new ObjCBaseListener() {
+        override def enterEveryRule(ctx: ParserRuleContext): Unit = {
+          lines +=
+            (ctx.depth - 1) + "  " * ctx.depth +
+            parser.getRuleNames()(ctx.getRuleIndex) + ": " + "'" + ctx.getStart.getText.replace("\n\r\t", " ") + "'"
+        }
+      }, root)
+    }
+
+    "/* " + lines.result.mkString("\n * ") + "\n */"
   }
 }
