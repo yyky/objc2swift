b79a01f4a85a86c9ee0f1f9323d192bfcdca8c80
Yuya Kamada <ykamada@yahoo-corp.jp>
2015-05-19 17:51:30 +0900

    Refactoring

--- src/main/scala/MethodVisitor.scala
+++ src/main/scala/MethodVisitor.scala
@@ -23,20 +23,43 @@ trait MethodVisitor extends Converter {
    * @param ctx the parse tree
    * @return Strings of Swift's instance method code
    */
-  override def visitInstance_method_declaration(ctx: Instance_method_declarationContext): String = {
-    val dcl = Option(ctx.method_declaration()).map(visit).getOrElse("")
-    s"${indent(ctx)}${optional(ctx)}func$dcl"
-  }
+  override def visitInstance_method_declaration(ctx: Instance_method_declarationContext): String =
+    Option(ctx.method_declaration()).map { c =>
+      s"${indent(ctx)}${optional(ctx)}${visit(c)}" stripSuffix " "
+    } getOrElse ""
 
   /**
    * Returns translated text of class method declaration context.
    *
    * @param ctx the parse tree
    **/
-  override def visitClass_method_declaration(ctx: Class_method_declarationContext): String = {
-    val dcl = Option(ctx.method_declaration()).map(visit).getOrElse("")
-    s"${indent(ctx)}${optional(ctx)}class func$dcl"
-  }
+  override def visitClass_method_declaration(ctx: Class_method_declarationContext): String =
+    Option(ctx.method_declaration()).map { c =>
+      s"${indent(ctx)}${optional(ctx)}class ${visit(c)}" stripSuffix " "
+    } getOrElse ""
+
+  /**
+   * Returns translated text of method definition context.
+   *
+   * @param ctx the parse tree
+   * @return Strings of Swift code
+   */
+  override def visitInstance_method_definition(ctx: Instance_method_definitionContext): String =
+    ctx.method_definition() match {
+      case c if !isVisited(c) => s"${indent(ctx)}${visit(c)}" stripSuffix " "
+      case _ => "" // Already printed
+    }
+
+  /**
+   * Returns translated text of class method definition context.
+   *
+   * @param ctx the parse tree
+   **/
+  override def visitClass_method_definition(ctx: Class_method_definitionContext): String =
+    ctx.method_definition() match {
+      case c if !isVisited(c) => s"${indent(ctx)}class ${visit(c)}" stripSuffix " "
+      case _ => "" // Already printed
+    }
 
   /**
    * Returns translated text of method declaration context.
@@ -49,9 +72,9 @@ trait MethodVisitor extends Converter {
       case Some(impl: Method_definitionContext) => visit(impl)
       case _ =>
         // Has no definition
-        val method_selector = ctx.method_selector()
-        val method_type = Option(ctx.method_type())
-        val hd = createMethodHeader(method_selector, method_type)
+        val slct = ctx.method_selector()
+        val tp = Option(ctx.method_type())
+        val hd = createMethodHeader(slct, tp)
 
         // Check ancestor is protocol or not
         ctx.parent.parent.parent match {
@@ -64,79 +87,39 @@ trait MethodVisitor extends Converter {
    * Returns translated text of method definition context.
    *
    * @param ctx the parse tree
-   * @return Strings of Swift code
-   */
-  override def visitInstance_method_definition(ctx: Instance_method_definitionContext): String =
-    isVisited(ctx.method_definition()) match {
-      case true => "" // Already printed
-      case false => s"${indent(ctx)}func${visit(ctx.method_definition())}"
-    }
-
-  override def visitClass_method_definition(ctx: Class_method_definitionContext): String =
-    isVisited(ctx.method_definition()) match {
-      case true => "" // Already printed
-      case false => s"${indent(ctx)}class func${visit(ctx.method_definition())}"
-    }
-
+   **/
   override def visitMethod_definition(ctx: Method_definitionContext): String = {
-    val sb = new StringBuilder
-    val method_selector: Method_selectorContext = ctx.method_selector()
-    val method_type: Option[Method_typeContext] = Option(ctx.method_type())
+    val builder = List.newBuilder[String]
+    val slct = ctx.method_selector()
+    val tp = Option(ctx.method_type())
+    val hd = createMethodHeader(slct, tp)
 
-    sb.append(createMethodHeader(method_selector, method_type))
-    sb.append(" {\n")
-    sb.append(visit(ctx.compound_statement()) + "\n")
-    sb.append(indent(ctx) + "}")
+    builder += s"$hd {\n"
+    builder += visit(ctx.compound_statement())
+    builder += s"${indent(ctx)}}"
 
-    sb.toString()
-  }
-
-  def createMethodHeader(sctx: Method_selectorContext, tctx: Option[Method_typeContext]): String = {
-
-    val sb = new StringBuilder()
-
-    // Method selector and parameters
-    sb.append(" " + visit(sctx))
-
-    //
-    // Method type.
-    //
-    // Currently, supported types are:
-    //   void, id(AnyObject), short/int/long, float/double, ...
-    //
-    // TODO: Support more types
-    //
-    tctx match {
-      case None => sb.append(" -> AnyObject") // Default
-      case Some(c) => visit(c) match {
-        case s if !s.isEmpty => sb.append(" -> " + s)
-        case _            => // void
-      }
-    }
-
-    sb.toString()
-  }
-
-  override def visitMethod_selector(ctx: Method_selectorContext): String = {
-    val sb = new StringBuilder()
-
-    Option(ctx.selector()) match {
-      case Some(s) => sb.append(visit(s) + "()") // No parameters
-      case _ =>
-        Option(ctx.keyword_declarator()) match {
-          case None => // TODO: Syntax error
-          case Some(ctxs) => ctxs.zipWithIndex.foreach {
-            case (c, 0) => sb.append(visit(c).format("("))
-            case (c, i) => sb.append(", " + visit(c).format(" "))
-          }
-        }
-        sb.append(")")
-    }
-    sb.toString()
+    builder.result().mkString
   }
 
   /**
-   * Construtor of method parameter.
+   * Returns translated text of method selector context.
+   * @param ctx the parse tree
+   **/
+  override def visitMethod_selector(ctx: Method_selectorContext): String =
+    Option(ctx.selector()) match {
+      case Some(s) => s"${visit(s)}()" // No parameters
+      case _ =>
+        val builder = List.newBuilder[String]
+        ctx.keyword_declarator().zipWithIndex.foreach {
+          case (c, 0) => builder += visit(c).format("(")
+          case (c, i) => builder += s", ${visit(c).format(" ")}"
+        }
+        builder += ")"
+        builder.result().mkString
+    }
+
+  /**
+   * Returns translated text of keyword declarator
    *
    * @param ctx the parse tree
    * @return parameter code
@@ -159,7 +142,7 @@ trait MethodVisitor extends Converter {
       case _                   => selector + "%s"
     }
 
-    sep + paramName + ": " + paramType
+    s"$sep$paramName: $paramType"
   }
 
   /**
@@ -168,13 +151,31 @@ trait MethodVisitor extends Converter {
    * @param ctx the parse tree
    * @return Swift method type
    */
-  override def visitMethod_type(ctx: Method_typeContext): String =
-    Option(ctx.type_name.specifier_qualifier_list) match {
-      case Some(s) =>
-        Option(s.type_specifier()).map(concatType(_)).getOrElse("AnyObject") match {
-          case "void" => ""
-          case t => t
-        }
-      case _ => "AnyObject"
+  override def visitMethod_type(ctx: Method_typeContext): String = {
+    val retType = (for {
+      x <- Option(ctx.type_name().specifier_qualifier_list())
+      y <- Option(x.type_specifier())
+    } yield y).map(concatType(_)).getOrElse("AnyObject")
+
+    retType match {
+      case "void" => ""
+      case _      => retType
     }
+  }
+
+  /**
+   * Returns method header text.
+   * @param sctx method_selector_context tree
+   * @param tctx method_type_context tree (Optional)
+   * @return Translated text of method header contexts.
+   */
+  private def createMethodHeader(sctx: Method_selectorContext, tctx: Option[Method_typeContext]): String =
+    tctx match {
+      case Some(c) => visit(c) match {
+        case s if !s.isEmpty => s"func ${visit(sctx)} -> $s"
+        case _               => s"func ${visit(sctx)}" // void
+      }
+      case None              => s"func ${visit(sctx)} -> AnyObject" // Default
+    }
+
 }
