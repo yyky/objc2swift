7cdac06322b103e4da2b282214784bd3eb71ef35
takesano <takesano@yahoo-corp.jp>
2015-07-03 19:29:00 +0900

    add raw-output feature

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -25,15 +25,16 @@ class Application extends Controller {
     """.stripMargin
 
   def index = Action { implicit request =>
-    val input = (for {
-      form <- request.body.asFormUrlEncoded
-      buff <- form.get("source")
-      if (buff.length > 0)
-    } yield buff(0)).getOrElse(defaultInput)
+    val form = request.body.asFormUrlEncoded
+    val raw = form.flatMap(_.get("raw")).map(_(0) == "1").getOrElse(false)
+    val input = form.flatMap(_.get("source")).map(_(0)).getOrElse(defaultInput)
 
     val converter = new ObjC2SwiftConverter(input)
     val result = converter.getResult
 
-    Ok(views.html.index("suzak", input, result))
+    if(!raw)
+      Ok(views.html.index("objc2swift-web", input, result))
+    else
+      Ok(result).as(HTML)
   }
 }
