3cbb94190d37ce35cd7bb65e1b28bd9fc075fe4b
takesano <takesano@yahoo-corp.jp>
2015-07-03 19:29:17 +0900

    add auto-convert feature.

--- web/public/javascripts/hello.js
+++ web/public/javascripts/hello.js
@@ -15,3 +15,27 @@ var swiftEditor = CodeMirror.fromTextArea(document.getElementById('editorSwift')
     matchBrackets: true,
     styleActiveLine: true
 });
+
+objcEditor.on('change', function() {
+  var currentTimer;
+  return function(obj, e) {
+    if(!$("#auto").is(':checked'))
+      return;
+
+    var timer = setTimeout(function() {
+      if(currentTimer !== timer) console.log("pass");
+
+      if(currentTimer !== timer)
+        return;
+
+      var input = e.text;
+      var value = obj.getValue();
+
+      $.post("", {source: value, raw: "1"}, function(data) {
+        console.log("update");
+        swiftEditor.getDoc().setValue(data);
+      })
+    }, 800);
+    currentTimer = timer;
+  }
+}());
