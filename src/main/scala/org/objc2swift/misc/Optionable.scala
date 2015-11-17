f2767f7ff35c55f8e145b5c179be1184ecd343e1
takesano <takesano@yahoo-corp.jp>
2015-11-17 16:10:49 +0900

    AnyRef に toOption を追加するのをやめ、Tree 型に狭めて追加するように修正。

--- src/main/scala/org/objc2swift/misc/Optionable.scala
+++
@@ -1,9 +0,0 @@
-package org.objc2swift.misc
-
-import scala.language.implicitConversions
-
-package object optionable {
-  implicit class Optionable[T <: AnyRef](value: T) {
-    def toOption: Option[T] = Option(value)
-  }
-}
