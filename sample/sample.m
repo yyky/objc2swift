539e854472dc7ebf9a5fbf63a0e2602db8bc0f18
takesano <takesano@yahoo-corp.jp>
2015-11-18 15:32:26 +0900

    simplified sample source.

--- sample/sample.m
+++ sample/sample.m
@@ -1,64 +1,8 @@
-@interface MyClass()
-
-@property (nonatomic, readwrite) BOOL loadingContentInfo;
-@property NSString *privateProp;
-
-- (void)privateMethod;
-
-@end
-
 @implementation MyClass
 
 - (void)doSomething
 {
-    [self somethingWithArg1:@"hello" arg2:0];
-    NSString formattedText = [NSString stringWithFormat:@"%@ %@", rawResult[@"FirstName"], rawResult[@"LastName"]];
-    entity.access = [[NSString alloc] initWithFormat:@"%@駅から徒歩%@分", nearestStation, time];
-    YSSSearchResultSet *resultSet = [[YSSSearchResultSet alloc] init];
-    MyClass *myClass = [[MyClass alloc]initWithName:@"Snoopy"];
-    UIMenuController.sharedMenuController.menuItems = @[[[UIMenuItem alloc] initWithTitle:@"検索" action:@selector(search:)]];
-
-    return [NSURL URLWithString:kYSSWebPageCaptureEndpointURL
-                  getParams:@{
-                      @"url": URL.absoluteString,
-                      @"w": @(size.width),
-                      @"h": @(size.height),
-                      @"s": @"s",
-                      @"device": iPhone() ? @"sp" : @"tb",
-                      @"width":@(width * kStaticMapRatio)
-                  }];
-}
-
-- (NSString *)somethingWithArg1:(id)arg1 arg2:(int)arg2
-{
-    return @"something";
-}
-
-+ (void)classMethod
-{
-    @"classMethod";
-}
-
-- (void)privateMethod
-{
-    @"private";
-}
-
-//- (void)funcA:(NSString *)p1 p2:(void (^)(id, NSError *))p2
-//{
-//}
-
-- (NSURL *)faviconURL
-{
-    return _faviconURL ?: _linkURL ? [YSSURLGenerator faviconURLForURL:_linkURL] : nil;
-}
-
-@end
-
-@implementation MyClass(Category1)
-
-- (void)category1Method
-{
+    NSLog(@"doSomething");
 }
 
 @end
