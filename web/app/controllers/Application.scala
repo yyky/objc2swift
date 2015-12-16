e60534346c3149fd0b835a9278f95d0d2243edbb
takesano <takesano@yahoo-corp.jp>
2015-12-16 22:09:35 +0900

    improve code, convert-button async.

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -6,9 +6,29 @@ import org.objc2swift.converter.ObjC2SwiftConverter
 
 class Application extends Controller {
 
+  def index = Action { implicit request =>
+    Ok(views.html.index("objc2swift-web", defaultInput, convertSource(defaultInput)))
+  }
+
+  def convert = Action { implicit request =>
+    val result = {
+      for {
+        form <- request.body.asFormUrlEncoded
+        input <- form.get("source").flatMap(_.headOption)
+      } yield convertSource(input)
+    } getOrElse ""
+
+    Ok(result).as(HTML)
+  }
+
+  private def convertSource(source: String): String = {
+    val parser = ObjC2SwiftConverter.generateParser(source)
+    val converter = new ObjC2SwiftConverter(parser)
+    converter.getResult()
+  }
+
   private val defaultInput =
-    """
-      |@interface MyClass
+    """@interface MyClass
       |
       |- (void)sayHello;
       |
@@ -20,21 +40,6 @@ class Application extends Controller {
       |    NSLog(@"Hello Swift, Goodbye Obj-C!");
       |}
       |
-      |@end
-      |
-    """.stripMargin
+      |@end""".stripMargin
 
-  def index = Action { implicit request =>
-    val form = request.body.asFormUrlEncoded
-    val raw = form.flatMap(_.get("raw")).map(_(0) == "1").getOrElse(false)
-    val input = form.flatMap(_.get("source")).map(_(0)).getOrElse(defaultInput)
-    val parser = ObjC2SwiftConverter.generateParser(input)
-    val converter = new ObjC2SwiftConverter(parser)
-    val result = converter.getResult
-
-    if(!raw)
-      Ok(views.html.index("objc2swift-web", input, result))
-    else
-      Ok(result).as(HTML)
-  }
 }
